93a550b1365d88ecba72ed85250b9fe3ae0f5f2c
Henrique Chaves <canaldomonza@gmail.com>
2023-02-07 13:04:28 -0300

    feat: add ENVs on project

--- src/data/database/connectionDB.js
+++ src/data/database/connectionDB.js
@@ -1,11 +1,11 @@
 import mysql from "mysql2/promise";
 
 const connDB = await mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "12345",
-  database: "cms",
-  port: "3306",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 export default connDB;
